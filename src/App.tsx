90e0462acb951a0523f0244da9577868d8f2786c
Serap Cakmak <serap.cakmak@hotmail.com>
2025-03-17 11:38:30 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { Button } from "@/components/ui/button";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
+const App = () => {
+  return (
+    <>
+      <div className="bg-slate-500">App</div>
+      <hr />
+      <Button>Button</Button>
+      <hr />
+      <Accordion type="single" collapsible>
+        <AccordionItem value="item-1">
+          <AccordionTrigger>Is it accessible?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It adheres to the WAI-ARIA design pattern.
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+      <hr />
+    </>
+  );
+};
+
+export default App;
