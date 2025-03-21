b8f806e9d3af0a392d4c659a91c817974f23b03f
Serap Cakmak <serap.cakmak@hotmail.com>
2025-03-21 11:01:20 +0100

    https://fringe-romantic-dinosaur.glitch.me

--- src/store/api/todoApi.ts
+++ src/store/api/todoApi.ts
@@ -1,7 +1,7 @@
 import type { Todo } from "../slices/todoSlice";
 import type { Category } from "../todoStore";
 
-const API_URL = "http://localhost:3001";
+const API_URL = "https://fringe-romantic-dinosaur.glitch.me";
 
 interface FetchTodosResponse {
   todos: Todo[];
